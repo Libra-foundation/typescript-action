56182bd0f7b023fb0f2f9645870f280d5fadfc2e
Galyfray <galyfray@gmail.com>
2023-01-20 23:16:16 +0100

    fixed tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,4 @@
-import {wait} from '../src/wait'
+import {Wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -6,12 +6,12 @@ import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+  await expect(Wait(input)).rejects.toThrow('milliseconds not a number')
 })
 
 test('wait 500 ms', async () => {
   const start = new Date()
-  await wait(500)
+  await Wait(500)
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
