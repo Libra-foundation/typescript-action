3ec1875379cf8ea1218ef9a88af1a019e49f90fc
galyfray <37240229+galyfray@users.noreply.github.com>
2023-01-21 13:17:58 +0100

    [REFATOR] Used our pre defined worflows and rules (#10)
    ## 1) Description
    
    Changed the state of the repo to use the workflows and ESLint rules that
    we defined in the React template repository.
    
    A workflow has been fixed due to a bug from jest. A few tweaks have been
    applied and will be propagated to the React template.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,17 +1,18 @@
-import {wait} from '../src/wait'
+import {Wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as fs from 'fs'
 import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+  await expect(Wait(input)).rejects.toThrow('milliseconds not a number')
 })
 
 test('wait 500 ms', async () => {
   const start = new Date()
-  await wait(500)
+  await Wait(500)
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
@@ -22,6 +23,11 @@ test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+
+  if (!fs.existsSync(ip)) {
+    cp.execSync("npm run build")
+  }
+  
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
